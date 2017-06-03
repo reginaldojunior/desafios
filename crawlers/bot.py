26cf82c05b4e162f0cbed484a4fdeafab841550d
Reginaldo Junior <76regi@gmail.com>
2017-06-03 19:51:34 -0300

    end bot and crawl

--- crawlers/bot.py
+++ crawlers/bot.py
@@ -1,40 +1,22 @@
+import os
 from telegram.ext import Updater, CommandHandler
 
 def start(bot, update):
-    update.message.reply_text('Hello World!')
-	
-    """
-        Shows an welcome message and help info about the available commands.
-    """
-    me = bot.get_me()
+    update.message.reply_text('Start using /NadaPraFazer')
 
-    # Welcome message
-    msg = "Hello!\n"
-    msg += "I'm {0} and I came here to help you.\n".format(me.first_name)
-    msg += "What would you like to do?\n\n"
-    msg += "/support - Opens a new support ticket\n"
-    msg += "/settings - Settings of your account\n\n"
+def nadaprafazer(bot, update):
+    message = update.message.text
+    message = message.split(' ')
+    reddits = message[1].split(';')
 
-    # Commands menu
-    main_menu_keyboard = [[telegram.KeyboardButton('/support')],
-                          [telegram.KeyboardButton('/settings')]]
-    reply_kb_markup = telegram.ReplyKeyboardMarkup(main_menu_keyboard,
-                                                   resize_keyboard=True,
-                                                   one_time_keyboard=True)
-
-    # Send the message with menu
-    bot.send_message(chat_id=update.message.chat_id,
-                     text=msg,
-                     reply_markup=reply_kb_markup)
-
-def hello(bot, update):
-    update.message.reply_text(
-        'Hello {}'.format(update.message.from_user.first_name))
+    for reddit in reddits:
+        update.message.reply_text('Crawling data from...' + reddit)    
+        os.system("scrapy runspider reddit.py -a subreddit=r/" + str(reddit) + " -a chat_id=" + str(update.message.chat.id) + " &> /dev/null &")        
 
 updater = Updater('390538628:AAFEBJzvhs2OZky4UCPUt4vLH21RbVjU2dg')
-
+    
 updater.dispatcher.add_handler(CommandHandler('start', start))
-updater.dispatcher.add_handler(CommandHandler('hello', hello))
+updater.dispatcher.add_handler(CommandHandler('NadaPraFazer', nadaprafazer))
 
 updater.start_polling()
 updater.idle()
--- crawlers/reddit.py
+++ crawlers/reddit.py
@@ -1,27 +1,48 @@
+# -*- coding: utf-8 -*-
+
 import scrapy
+import requests
+import urllib
 from scrapy.selector import Selector
 from scrapy.http import HtmlResponse
 
+
 class RedditSpider(scrapy.Spider):
     name = 'redditspider'
-    subreddit = 'r/brasil'
-    start_urls = ['https://www.reddit.com/' + subreddit +'/top/?sort=top&t=year']
+    subreddit = ''
+    chat_id = ''
+    endpoint = 'https://api.telegram.org/bot390538628:AAFEBJzvhs2OZky4UCPUt4vLH21RbVjU2dg/sendMessage'
+
+    def __init__(self, subreddit='r/brasil', chat_id='', *args, **kwargs):
+        super(RedditSpider, self).__init__(*args, **kwargs)
+
+        self.subreddit = subreddit
+        self.chat_id = chat_id
+        self.start_urls = ['https://www.reddit.com/' +
+                           subreddit + '/top/?sort=top&t=year']
 
     def parse(self, response):
-		selected = Selector(response)	
-		reddits = selected.css('div.link')
-		
-		for reddit in reddits:
-			upvotes = reddit.css('div.likes::text').extract_first()
+        selected = Selector(response)
+        reddits = selected.css('div.link')
 
-			if int(upvotes) >= 5000:
-				yield {
-					'upvotes': upvotes,
-					'title': reddit.css('p.title a::text').extract_first(),
-					'link': reddit.css('p.title a::attr(href)').extract_first(),
-					'subreddit': self.subreddit,
-					'comments': reddit.css('li.first a::attr(href)').extract_first()
-				}
+        for reddit in reddits:
+            upvotes = False
 
-		for next_page in response.css('span.next-button > a'):
-			yield response.follow(next_page, self.parse)
+            try:
+                upvotes = int(reddit.css('div.likes::text').extract_first())
+            except Exception, e:
+                upvotes = True
+
+            if upvotes >= 5000 or upvotes is True:
+                text = '*Up Votes*: ' + \
+                    reddit.css('div.likes::text').extract_first() + '\n'
+                text += '*Titulo*: ' + \
+                    reddit.css('p.title a::text').extract_first() + '\n'
+                text += '*Link*: [Acesse o Reddit](https://www.reddit.com/' + reddit.css(
+                    'p.title a::attr(href)').extract_first() + ')\n'
+                text += '*SubReddit*: [Veja todos Reddits](https://www.reddit.com/' + \
+                    self.subreddit + ')\n'
+                text += '*Comments*: [Comentarios do Reddit](' + reddit.css(
+                    'li.first a::attr(href)').extract_first() + ')'
+
+                yield requests.get(self.endpoint + '?chat_id=' + self.chat_id + '&text=' + text + '&parse_mode=Markdown')
