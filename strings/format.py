4d30eb73744dac71ec65114a02f6b4b3585a7bb2
Reginaldo Junior <76regi@gmail.com>
2017-06-04 00:15:29 -0300

    refactor code

--- strings/format.py
+++ strings/format.py
@@ -1,13 +1,22 @@
+import re
+
 out = open("out.txt", "w")
 
 last = 40
+lineString = ""
 with open("input.txt", "r") as _input:
+    
     for line in _input:
-        for i in range(len(line)):
-            if i % 40 == 0:
-                outtxt = line[last:i]
-                last = i
-                out.write(outtxt)
+        words = re.split('\s+', line)
+
+        for word in words:
+            if len(lineString) <= 40:
+                lineString += word + " "
+            else:
                 out.write("\n")
+                out.write(lineString)
+                lineString = ""
+
+        out.write("\n")
 
 out.close()
